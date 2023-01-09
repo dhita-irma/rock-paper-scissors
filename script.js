b0e780cf333d874d915101a5c8220d4bfc026893
dhita-irma <52138969+dhita-irma@users.noreply.github.com>
2023-01-09 14:14:10 -0800

    Create function playRound() and game()
    Function playRound() plays a single round of Rock Paper Scissors, taking two parameters playerSelection and computerSelection and return the result
    
    Function game() will play a game consists of five rounds

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
-// Create function getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+const hands = ["ROCK", "PAPER", "SCISSORS"];
+
+// Get computer choice, return either ‘Rock’, ‘Paper’ or ‘Scissors’
 function getComputerChoice() {
-    const hands = ['Rock', 'Paper', 'Scissors'];
-    return hands[Math.floor(Math.random()*hands.length)];
+  return hands[Math.floor(Math.random() * hands.length)];
 }
 
-console.log(getComputerChoice());
+// Play single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  // Compare playerSelection vs computerSelection
+  console.log(`You: ${playerSelection} vs Computer: ${computerSelection}`);
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else {
+    switch (`${playerSelection} vs ${computerSelection}`) {
+      case "ROCK vs SCISSORS":
+        return "You won! Rock beats Scissors";
+      case "ROCK vs PAPER":
+        return "You lose! Paper beats Rock.";
+      case "PAPER vs ROCK":
+        return "You won! Paper beats Rock.";
+      case "PAPER vs SCISSORS":
+        return "You lose! Scissors beats Paper";
+      case "SCISSORS vs PAPER":
+        return "You won! Scissors beats Paper";
+      case "SCISSORS vs ROCK":
+        return "You lose! Rock beats Scissors";
+    }
+  }
+}
+
+// A game consist of 5 rounds
+function game() {
+  let playerSelection;
+  let computerSelection;
+
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    // Prompt player for selection
+    while (!hands.includes(playerSelection)) {
+      playerSelection = prompt().toUpperCase();
+    }
+
+    // Get computer selection
+    computerSelection = getComputerChoice();
+
+    // Play a round
+    console.log(`Round ${i + 1}`)
+    console.log(playRound(playerSelection, computerSelection));
+
+    // Reset selections 
+    playerSelection = "";
+    computerSelection = "";
+
+
+  }
+}
+
+game();
