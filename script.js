6261a5bab07cc5c5cab9360239696b226de7c915
dhita-irma <52138969+dhita-irma@users.noreply.github.com>
2023-01-31 09:33:03 -0800

    Announce the winner of the game
    - Check if one of the players has reached 5 points
    - End the game and announce the winner
    - Buttons will not do anything when clicked

--- script.js
+++ script.js
@@ -42,36 +42,45 @@ const computerWeapon = document.getElementById('computer-weapon');
 const playerScore = document.getElementById('player-score');
 const computerScore = document.getElementById('computer-score');
 const result = document.getElementById('result');
-const explanation = document.getElementById('explanation')
+const explanation = document.getElementById('explanation');
 
 let playerScoreNum = 0;
 let computerScoreNum = 0;
 
 weaponsBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    // Get selections and display
-    const playerSelection = button.id;
-    const computerSelection = getComputerChoice()
-    playerWeapon.textContent = button.textContent;
-    computerWeapon.textContent = convertIcon(computerSelection);
-
-    // Play round and display result
-    const roundResult = playRound(playerSelection, computerSelection);
-    result.textContent = roundResult[1];
-    explanation.textContent = roundResult[2];
-
-    // Calculate score 
-    if (roundResult[0] === "WIN") {
-      playerScoreNum++;
-    } else if (roundResult[0] === "LOSE") {
-      computerScoreNum++;
-    } else if (roundResult[0] === "TIE") {
-      playerScoreNum++;
-      computerScoreNum++;
+    if (playerScoreNum < 5 && computerScoreNum < 5) {
+      // Get selections and display
+      const playerSelection = button.id;
+      const computerSelection = getComputerChoice()
+      playerWeapon.textContent = button.textContent;
+      computerWeapon.textContent = convertIcon(computerSelection);
+  
+      // Play round and display result
+      const roundResult = playRound(playerSelection, computerSelection);
+      result.textContent = roundResult[1];
+      explanation.textContent = roundResult[2];
+  
+      // Calculate score 
+      if (roundResult[0] === "WIN") {
+        playerScoreNum++;
+      } else if (roundResult[0] === "LOSE") {
+        computerScoreNum++;
+      } else if (roundResult[0] === "TIE") {
+        playerScoreNum++;
+        computerScoreNum++;
+      }
+  
+      // Display Score 
+      playerScore.textContent = `You: ${playerScoreNum.toString()}`;
+      computerScore.textContent = `Computer: ${computerScoreNum.toString()}`;
+    } else {
+      result.textContent = "5 Points Reached";
+      if (playerScoreNum == 5) {
+        explanation.textContent = "CONGRATULATIONS, YOUT WIN!";
+      } else {
+        explanation.textContent = "SORRY, YOU LOSE!";
+      }
     }
-
-    // Display Score 
-    playerScore.textContent = `You: ${playerScoreNum.toString()}`;
-    computerScore.textContent = `Computer: ${computerScoreNum.toString()}`;
   })
 });
