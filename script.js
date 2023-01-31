4b5a10d038676fdf19a1fcccc9a4d07b55be36b3
dhita-irma <52138969+dhita-irma@users.noreply.github.com>
2023-01-30 16:06:10 -0800

    Display the running score
    Display player score and computer score. At this point, the game can
    still run indefinitely.

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function getComputerChoice() {
 // Play single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return ["It's a tie!", "Both player get one point."];
+    return ["TIE", "It's a tie!", "Both player get one point."];
   } else {
     switch (`${playerSelection} vs ${computerSelection}`) {
       case "ROCK vs SCISSORS":
       case "PAPER vs ROCK":
       case "SCISSORS vs PAPER":
-        return ["You Win!", "You get one point."];
+        return ["WIN", "You Win!", "You get one point."];
       case "ROCK vs PAPER":
       case "PAPER vs SCISSORS":
       case "SCISSORS vs ROCK":
-        return ["You Lose!", "Computer gets one point."];
+        return ["LOSE", "You Lose!", "Computer gets one point."];
     }
   }
 }
@@ -39,19 +39,39 @@ function convertIcon(weapon) {
 const weaponsBtn = document.querySelectorAll('.btn');
 const playerWeapon = document.getElementById('player-weapon');
 const computerWeapon = document.getElementById('computer-weapon');
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
 const result = document.getElementById('result');
 const explanation = document.getElementById('explanation')
 
+let playerScoreNum = 0;
+let computerScoreNum = 0;
+
 weaponsBtn.forEach((button) => {
   button.addEventListener('click', () => {
+    // Get selections and display
     const playerSelection = button.id;
     const computerSelection = getComputerChoice()
-
     playerWeapon.textContent = button.textContent;
     computerWeapon.textContent = convertIcon(computerSelection);
 
+    // Play round and display result
     const roundResult = playRound(playerSelection, computerSelection);
-    result.textContent = roundResult[0];
-    explanation.textContent = roundResult[1];
+    result.textContent = roundResult[1];
+    explanation.textContent = roundResult[2];
+
+    // Calculate score 
+    if (roundResult[0] === "WIN") {
+      playerScoreNum++;
+    } else if (roundResult[0] === "LOSE") {
+      computerScoreNum++;
+    } else if (roundResult[0] === "TIE") {
+      playerScoreNum++;
+      computerScoreNum++;
+    }
+
+    // Display Score 
+    playerScore.textContent = `Player: ${playerScoreNum.toString()}`;
+    computerScore.textContent = `Computer: ${computerScoreNum.toString()}`;
   })
 });
